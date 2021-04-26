ce5ea8dadc72fec5b047755f9ea709bef9ff74d8
Tyler Walborn <im.t.wal@gmail.com>
2021-04-25 20:27:54 -0600

    WIP: scroll loading and fix header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { GetRecent } from '../api/flickr';
 import { iImage } from '../consts/types';
 import Container from '../layouts/Container';
@@ -6,16 +7,36 @@ import Images from './Images';
 
 const Home: React.FC = () => {
   const [images, setImages] = useState<Array<iImage> | null>(null);
-  const getImages = async (): Promise<void> => {
-    setImages(await GetRecent(0, 20));
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const getImages = async (pageNumber: number): Promise<void> => {
+    const imgs = await GetRecent(pageNumber, 20);
+    setImages((previousImages) => {
+      if (previousImages) {
+        return [...previousImages, ...imgs];
+      }
+      return imgs;
+    });
   };
+
+  const fetchNextPage = (): void => {
+    setPageNumber((previousPageNum) => previousPageNum + 1);
+  };
+
   useEffect(() => {
-    getImages();
-  }, []);
+    getImages(pageNumber);
+  }, [pageNumber]);
 
   return (
     <Container>
-      <Images images={images} />
+      <InfiniteScroll
+        dataLength={images?.length || 0}
+        hasMore
+        next={fetchNextPage}
+        loader={<div>Sure</div>}
+      >
+        <Images images={images} />
+      </InfiniteScroll>
     </Container>
   );
 };
